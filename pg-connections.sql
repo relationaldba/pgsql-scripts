988218121d60e40b89e1101cbe52532b86050bd5
Varun Deshpande <varundeshpande.vd@gmail.com>
2023-08-28 15:24:58 -0400

    Added a couple of scripts along with the gitignore file

---
+++ pg-connections.sql
@@ -0,0 +1,18 @@
+SELECT
+    datname,
+    usename,
+    client_addr::text,
+    application_name,
+    COUNT(*)
+FROM
+    pg_stat_activity
+WHERE
+    1 = 1
+    -- AND state <> 'idle'
+    -- AND application_name = 'PostgreSQL JDBC Driver'
+GROUP BY
+    datname,
+    usename,
+    client_addr::text,
+    application_name;
+
