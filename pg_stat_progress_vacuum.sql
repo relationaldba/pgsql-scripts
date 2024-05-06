fe0b21090a25d5dc27658bdc4698241b0d532f10
Varun Deshpande <varundeshpande.vd@gmail.com>
2024-05-05 20:56:51 -0400

    Updated scripts

--- pg_stat_progress_vacuum.sql
+++ pg_stat_progress_vacuum.sql
@@ -23,3 +23,27 @@ FROM
     JOIN pg_stat_activity a using (pid)
 ORDER BY 
     duration DESC;
+
+
+
+SELECT
+    p.pid,
+    age(current_timestamp, a.xact_start) AS duration,
+    coalesce(wait_event_type ||'.'|| wait_event, 'f') AS waiting,
+    CASE
+        WHEN a.query ~*'^autovacuum.*to prevent wraparound' THEN 'wraparound'
+        WHEN a.query ~*'^vacuum' THEN 'user'
+    ELSE 'regular'
+    END AS mode,
+    p.datname AS database,
+    p.relid::regclass AS table,
+    p.phase,
+    pg_size_pretty(p.heap_blks_total * current_setting('block_size')::int) AS table_size,
+    -- pg_size_pretty(pg_total_relation_size(relid)) AS total_size,
+    pg_size_pretty(p.heap_blks_scanned * current_setting('block_size')::int) AS scanned,
+    round(100.0 * p.heap_blks_scanned / p.heap_blks_total, 1) AS scanned_pct
+FROM 
+    pg_stat_progress_cluster p
+    LEFT JOIN pg_stat_activity a using (pid)
+ORDER BY 
+    duration DESC;
